34f4b900d515a35fa257eaa8b696aaa8dbf43e31
Fernando Núñez <fnunez@atsistemas.com>
2017-07-12 11:19:51 +0200

    Fix

--- src/main/java/com/docomo/test/Test.java
+++ src/main/java/com/docomo/test/Test.java
@@ -5,7 +5,7 @@ import redis.clients.jedis.Jedis;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Test Jedis 2");
 		
 		Jedis jedis = new Jedis("localhost");
 		jedis.set("foo", "bar");
